3a461f418e54a23f6ae1be03109162f3c0c786e3
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-01-17 17:11:45 +0300

    Adding OOP into project

--- main.py
+++ main.py
@@ -1,64 +1,23 @@
-import menu
-import coffee_maker
-import money_machine
-
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 
-def check_resources(order_ingredients):
-    for item in order_ingredients:
-        if order_ingredients[item] > resources[item]:
-            print(f"Sorry there is not enough {item}.")
-            return False
-    return True
-
-
-def process_coins():
-    print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
-
-
-def is_transaction_successful(money_received, drink_cost):
-    if money_received >= drink_cost:
-        change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change.")
-        global profit
-        profit += drink_cost
-        return True
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-        return False
-
-
-def make_coffee(drink_name, order_ingredients):
-    for item in order_ingredients:
-        resources[item] -= order_ingredients[item]
-    print(f"Here is your {drink_name} ☕️. Enjoy!")
-
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
 is_on = True
 
 while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${profit}")
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = MENU[choice]
-        if check_resources(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(choice, drink["ingredients"])
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
