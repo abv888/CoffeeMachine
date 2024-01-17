5adfcd64876c28f52007248b3c01fba0481cd53b
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-01-17 17:04:42 +0300

    classes

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-# MENU = {
-#     "espresso": {
-#         "ingredients": {
-#             "water": 50,
-#             "coffee": 18,
-#         },
-#         "cost": 1.5,
-#     },
-#     "latte": {
-#         "ingredients": {
-#             "water": 200,
-#             "milk": 150,
-#             "coffee": 24,
-#         },
-#         "cost": 2.5,
-#     },
-#     "cappuccino": {
-#         "ingredients": {
-#             "water": 250,
-#             "milk": 100,
-#             "coffee": 24,
-#         },
-#         "cost": 3.0,
-#     }
-# }
+import menu
+import coffee_maker
+import money_machine
 
 profit = 0
 resources = {
